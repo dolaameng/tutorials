b554e36d290c349219a4e872dabf6d7c6940f7af
ce <ce@ce-pc.(none)>
2013-05-14 17:22:47 +0800

    doing features

--- ml-tutorials/features.py
+++ ml-tutorials/features.py
@@ -3,4 +3,47 @@
 ## from a data matrix, and some other feature transformation methods (e.g., tri-kmeans)
 ## The extracted new feature patches can be persisted in the ensemble for ensemble building
 
+## TODO - ONLINE and OFFLINE two versions
 
+import numpy as np
+
+########### data partitioning ##################
+
+def patch(data, rows, cols = None):
+	"""
+	data = data matrix, 1D or 2D array (matrix) 
+	rows = iterator of rows (list) to select, None means selecting all rows
+	cols = iterator of cols (list) to select, None means selecting all cols 
+	return np.array (of the patch shape), but the DIM of return should be 
+	the same as data (1D or 2D)
+	"""
+	data = np.asarray(data)
+	dim = get_dim(data)
+	if dim == 1:
+		## ignore cols
+		return data[rows] if rows else data
+	elif dim == 2:
+		nrows, ncols = data.shape
+		rows = rows or xrange(nrows)
+		cols = cols or xrange(ncols)
+		return data[np.ix_(rows, cols)]
+	else:
+		raise RuntimeError('only supports 1D or 2D array') 
+
+########## sequence generator #################
+def strided_seqs(seq, stride, subsz):
+	"""
+	rng = the sequence to be selected from
+	stride = stride (diff) between different sub_seqs
+	subsz = the window size of all sub_seqs
+	return iterable of subseqs 
+	"""
+	pass
+
+
+##################### helper function ###############
+def get_dim(data):
+	"""
+	return the dimension of the data - 1D or 2D np.array
+	"""
+	return len(np.asarray(data).shape)

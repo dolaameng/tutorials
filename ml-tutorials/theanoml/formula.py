1ecd87d948410e219cce430a107876d695d02118
ce <ce@ce-power.(none)>
2013-05-22 16:34:27 +0800

    TODO: MLP theano

---
+++ ml-tutorials/theanoml/formula.py
@@ -0,0 +1,3 @@
+## collection of common formulas used for theano machine learning toolkit
+## Usually the formulas are written in a way that separating those depending on X
+## and those depending on y
